b6a6cef41e12eee97d82ecf69f026dd672332509
mrzalais <lejnieksedijs@gmail.com>
2020-11-21 13:33:22 +0200

    Added crud

---
+++ app/Models/Article.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Article
+ * @package App\Models
+ *
+ * @mixin Builder
+ */
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
